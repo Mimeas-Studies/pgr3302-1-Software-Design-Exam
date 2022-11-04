983f52d661b1e11dd7699f9fd1ee1e1820b6ecf3
andreasbaade <andreasbaade@hotmail.com>
2022-11-04 10:53:09 +0100

    IOManager.cs comments

--- TextAnalyzer/UI/IOManager.cs
+++ TextAnalyzer/UI/IOManager.cs
@@ -1,19 +1,23 @@
 namespace TextAnalyzer.UI;
-
+/// <summary>
+/// IOManager, user-provided objects, stores assets, outputs and loads them as inputs to downstream assets
+/// </summary>
 public class IOManager
 {
+//Input, writes user specified data to standard input stream, and reads the characters from the input stream
     public static string? Input(string text = "")
     {
         Console.WriteLine(text);
         var input = Console.ReadLine();
         return input;
     }
-
+    
+//Standard writeline, writes specified data to input stream
     public static void Write(string text)
     {
         Console.WriteLine(text); 
     }
-
+//Clears console screen and buffer of display information 
     public static void ClearConsole()
     {
         Console.Clear();
