55ba2f0ccc11069f034d50b55fa9c3ff0b817346
Langeland1996 <89384483+Langeland1996@users.noreply.github.com>
2022-11-11 11:26:50 +0100

    🔵 UI/MenuHandler is placed to optimized and placed inside MainManager.cs

--- TextAnalyzer/Program.cs
+++ TextAnalyzer/Program.cs
@@ -1,18 +1,4 @@
 ﻿using TextAnalyzer.Analyzer;
 
-namespace TextAnalyzer;
 
-public class Program
-{
-    internal static bool IsProgramRunning = true;
 
-    public static void Main(String[] args) {
-        //Infinite while loop of the main menu switch case, while isProgramRunning set to true,
-        //false value set to five in switch case, exiting program 
-        while (IsProgramRunning)
-        {
-            Ui.PrintMenu();
-            MenuHandler.menuHandler();
-        }
-    }
-}
