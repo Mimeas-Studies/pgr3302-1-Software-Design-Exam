f1ea245f3ab1d6b6918dc1702c658a0f05d4da8c
Snorre <snle001@student.kristiania.no>
2022-11-24 12:33:46 +0100

    Making ready for merge

--- TextAnalyzer/Analyzer/AnalyzerManager.cs
+++ TextAnalyzer/Analyzer/AnalyzerManager.cs
@@ -1,7 +1,7 @@
-namespace TextAnalyzer.Analyzer; 
+namespace TextAnalyzer.Analyzer;
 
-public class AnalyzerManager {
-
+public class AnalyzerManager
+{
     private static Queue<string> Text { get; set; } = new();
     private Thread[] Threads { get; }
     private AnalyzerResult[] Results { get; }
@@ -15,48 +15,28 @@ public class AnalyzerManager {
         _textStream = textStream;
     }
 
-    public AnalyzerResult StartAnalyze() {
-        
-        // Parallel.For(0, Threads.Length, i => {
-        //     Results[i] = new AnalyzerResult();
-        //     var analyzer = new AnalyzerThread(Results[i], Text);
-        //     
-        //     Threads[i] = new Thread(analyzer.Start);
-        //     Threads[i].Start();
-        // });
-
-        for (int i = 0; i < Threads.Length; i++) {
+    public AnalyzerResult StartAnalyze()
+    {
+        for (int i = 0; i < Threads.Length; i++)
+        {
             Results[i] = new AnalyzerResult();
             var analyzer = new AnalyzerThread(Results[i], _textStream);
-            
+
             Threads[i] = new Thread(analyzer.Start);
             Threads[i].Start();
         }
 
-        for (int i = 0; i < Threads.Length; i++) {
-            Threads[i].Join();
+        foreach (var thread in Threads)
+        {
+            thread.Join();
         }
 
-        // var isRunning = true;
-        // while (isRunning) {
-        //     int counter = 0;
-        //     
-        //     for (int i = 0; i < Threads.Length; i++) {
-        //         if (!Threads[i].IsAlive) counter++;
-        //     }
-        //
-        //     if (counter == Threads.Length) isRunning = false;
-        //     else {
-        //         Task.Wait(500);
-        //     } 
-        // }
-        
         var finishedResult = new AnalyzerResult();
-        foreach (var result in Results) {
+        foreach (var result in Results)
+        {
             finishedResult += result;
         }
-        
+
         return finishedResult;
     }
-
 }
--- TextAnalyzer/FileReader/FileManager.cs
+++ TextAnalyzer/FileReader/FileManager.cs
@@ -5,18 +5,20 @@ namespace TextAnalyzer;
 /// <summary>
 /// Handles txt files by storing them in Queue's, displays them from directories and return them
 /// </summary>
-public class FileManager {
+public class FileManager
+{
     private List<string>? _textFileArrayList;
     private List<string>? _textFileNames;
     private int _selectedFile;
     private bool _notValidInput;
-    
+
     /// <summary>
     /// Reads .txt files and adds strings separated by an empty space to a queue.
     /// </summary>
     /// <param name="filepath">takes in a file from  bin/debug/net6.0/resources</param>
     /// <returns>a list of strings in a queue</returns>
-    public static IEnumerator<string> GetText(string filepath) {
+    public static IEnumerator<string> GetText(string filepath)
+    {
         IEnumerable<string> text = File.ReadLines(filepath);
         return text.GetEnumerator();
     }
@@ -25,7 +27,8 @@ public class FileManager {
     /// Displays files that are located in the resources directory
     /// checks in user input is valid based on files from directory
     /// </summary>
-    internal void DisplayStoredFiles() {
+    internal void DisplayStoredFiles()
+    {
         _notValidInput = true;
         _textFileArrayList = new List<string>();
         _textFileNames = new List<string>();
@@ -35,7 +38,8 @@ public class FileManager {
         var directoryInfo = new DirectoryInfo("Resources"); //Insert directory
         FileInfo[] files = directoryInfo.GetFiles("*.txt"); //Get files the end with .txt
         var counter = 0;
-        foreach (FileInfo file in files) {
+        foreach (FileInfo file in files)
+        {
             counter++;
             Console.WriteLine(counter + ". " + file.Name);
             _textFileNames.Add(file.Name);
@@ -43,17 +47,21 @@ public class FileManager {
         }
 
         IOManager.Write("\nType in menu option number and press <Enter> to analyse text");
-        
+
         var inputInt = Convert.ToInt32(Console.ReadLine());
-        while (_notValidInput) {
-            if (inputInt > _textFileArrayList.Count || inputInt <= 0) {
+        while (_notValidInput)
+        {
+            if (inputInt > _textFileArrayList.Count || inputInt <= 0)
+            {
                 Console.WriteLine("Input to high, try again:");
                 inputInt = Convert.ToInt32(Console.ReadLine());
             }
-            else {
+            else
+            {
                 _notValidInput = false;
                 Console.Clear();
             }
+
             _selectedFile = inputInt;
         }
     }
@@ -62,17 +70,19 @@ public class FileManager {
     /// Checks if there are existing files added previously
     /// </summary>
     /// <returns> returns selected file path with user input </returns>
-    public string GetSelectedFile() {
+    public string GetSelectedFile()
+    {
         if (_textFileArrayList != null) return _textFileArrayList[_selectedFile - 1];
         return "No files are stored on disk";
     }
 
-    
+
     /// <summary>
     /// Checks if there are existing file names added previously
     /// </summary>
     /// <returns> returns selected file name with user input </returns>
-    public string RetriveAllFileNames() {
+    public string RetriveAllFileNames()
+    {
         if (_textFileNames != null) return _textFileNames[_selectedFile - 1];
         return "No files are stored on disk";
     }
